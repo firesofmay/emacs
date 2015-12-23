348c468fd794254839b14a4a12da6257aff85cec
Mayank Jain <mayank@helpshift.com>
2015-12-23 23:09:21 +0530

    Fix: Intelligently move the mark

--- init.el
+++ init.el
@@ -770,6 +770,15 @@
 
 (define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
 
+;; http://endlessparentheses.com/faster-pop-to-mark-command.html
+;; When popping the mark, continue popping until the cursor actually moves
+(defadvice pop-to-mark-command (around ensure-new-position activate)
+  (let ((p (point)))
+    (dotimes (i 10)
+      (when (= p (point)) ad-do-it))))
+
+(setq set-mark-command-repeat-pop t)
+
 ;; https://github.com/clojure-emacs/cider/issues/1479
 ;; Fix for "{" in clojure repl
 (add-hook 'cider-repl-mode-hook
